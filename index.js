4355d2d4864a6e2842b4e68f6001b9396a33e8c2
jackismoe <jacksonmo@me.com>
2020-09-18 11:45:29 -0400

    Lab Submit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your functions here
+const map = (source, callback) => {
+  let newArray = []
+  for (let i = 0; i < source.length; i++) {
+    let element = source[i]
+    newArray.push(callback(element))
+  }
+  return newArray
+}
+
+const reduce = (source, callback, start) => {
+  let x = (!!start) ? start : source[0]
+  for (let i = (!!start) ? 0 : 1; i < source.length; i++) {
+    x = callback(source[i], x)
+  }
+  return x
+}
